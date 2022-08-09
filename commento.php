63e9c2d3b781e6bca054b5924e7665fed0242182
Sara <saradegra@gmail.com>
2022-08-09 21:37:41 +0200

    commento php

--- commento.php
+++ commento.php
@@ -1,7 +1,22 @@
 <?php
 
-require("connDB.php");
+require("./connDB.php");
 $nome = $_GET["nome"];
 $email = $_GET["email"];
 $commento = $_GET["commento"];
+
+$database = new Database();
+$db = $database->connessione();
+
+$query = "INSERT INTO commenti (commento, nome_utente, email_utente) VALUES ('$commento', '$nome', '$email');";
+
+$x = $db->prepare($query);
+$x->execute();
+
+if($x){
+        echo "You sent successfully your review! Thank you!";
+    } else {
+        echo "smth went wrong ugh (commento.php)";
+    }
+
 ?>
