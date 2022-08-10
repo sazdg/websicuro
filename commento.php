1f1e5bbf9522b4fde07ad076dad105b6caf01eba
Sara <saradegra@gmail.com>
2022-08-10 23:15:51 +0200

    stile pagine

--- commento.php
+++ commento.php
@@ -4,11 +4,12 @@ require("./connDB.php");
 $nome = $_GET["nome"];
 $email = $_GET["email"];
 $commento = $_GET["commento"];
+$post = $_GET["post"];
 
 $database = new Database();
 $db = $database->connessione();
 
-$query = "INSERT INTO commenti (commento, nome_utente, email_utente) VALUES ('$commento', '$nome', '$email');";
+$query = "INSERT INTO commenti (commento, nome_utente, email_utente, post) VALUES ('$commento', '$nome', '$email', '$post');";
 
 $x = $db->prepare($query);
 $x->execute();
